0b32327b45df891c14f400c2f509f1854789572e
xiaowei-007 <302929314@qq.com>
2020-06-19 10:21:06 +0800

    我的第一个版本

---
+++ demo.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Abstarfish\Starfish\Tools;
+
+Tools::Hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Abstarfish\Starfish;
+
+
+class Tools
+{
+    public function __construct()
+    {
+    }
+
+    public static function Hello(){
+        echo "Hello world";
+    }
+}
