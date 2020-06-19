33cc65962a6e7f21ddebb539bd133c0d65261b4e
xiaowei-007 <302929314@qq.com>
2020-06-19 11:37:00 +0800

    添加Fish类

--- demo.php
+++ demo.php
@@ -3,5 +3,8 @@
 require_once './vendor/autoload.php';
 
 use Abstarfish\Starfish\Tools;
+use Abstarfish\Starfish\Fish;
 
 Tools::Hello();
+
+Fish::Hello();
---
+++ src/Abstarfish/Starfish/Fish.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Abstarfish\Starfish;
+
+
+class Fish
+{
+    public static function Hello (){
+        echo "Hello xiaowei";
+    }
+
+}
